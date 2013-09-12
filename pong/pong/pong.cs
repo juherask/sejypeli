d5470f0326f8ac79c6f3ff260cdf134350ff55cf
juherask <jussi.rasku@jyu.fi>
2013-09-12 08:16:49 -0700

    korjasin pongin

---
+++ pong/pong/pong.cs
@@ -0,0 +1,42 @@
+﻿using Jypeli;
+
+public class pong : PhysicsGame
+{
+    PhysicsObject pallo;
+
+    public override void Begin()
+    {
+        LuoKentta();
+        AloitaPeli();
+
+        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+    }
+
+    void LuoKentta()
+    {
+        pallo = new PhysicsObject(40.0, 40.0);
+        pallo.Shape = Shape.Circle;
+        pallo.X = -200.0;
+        pallo.Y = 0.0;
+        pallo.Restitution = 1.0;
+        Add(pallo);
+
+        PhysicsObject maila = PhysicsObject.CreateStaticObject(20.0, 100.0);
+        maila.Shape = Shape.Rectangle;
+        maila.X = Level.Left + 20.0;
+        maila.Y = 0.0;
+        maila.Restitution = 1.0;
+        Add(maila);
+
+        Level.CreateBorders(1.0, false);
+        Level.Background.Color = Color.Black;
+
+        Camera.ZoomToLevel();
+    }
+
+    void AloitaPeli()
+    {
+        Vector impulssi = new Vector(500.0, 0.0);
+        pallo.Hit(impulssi);
+    }
+}
