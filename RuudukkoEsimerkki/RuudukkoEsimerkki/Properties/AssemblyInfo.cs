c6f14542f65a32ec68b4c85df61456f96b335e97
juherask <jussi.rasku@jyu.fi>
2013-10-24 06:29:56 -0700

    -Ruudukkoesimerkin koodit.

---
+++ RuudukkoEsimerkki/RuudukkoEsimerkki/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RuudukkoEsimerkki")]
+[assembly: AssemblyProduct("RuudukkoEsimerkki")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("Seinäjoen kaupunki")]
+[assembly: AssemblyCopyright("Copyright © Seinäjoen kaupunki 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type. Only Windows
+// assemblies support COM.
+[assembly: ComVisible(false)]
+
+// On Windows, the following GUID is for the ID of the typelib if this
+// project is exposed to COM. On other platforms, it unique identifies the
+// title storage container when deploying this assembly to the device.
+[assembly: Guid("966c61b2-88c2-4966-9f11-6f914ee17a07")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ RuudukkoEsimerkki/RuudukkoEsimerkki/RuudukkoEsimerkki.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Jypeli;
+using Jypeli.Assets;
+using Jypeli.Controls;
+using Jypeli.Effects;
+using Jypeli.Widgets;
+
+public class RuudukkoEsimerkki : PhysicsGame
+{
+    Image pelaajaKuva = LoadImage("pelaaja");
+
+    PhysicsObject LuoBetoniseina()
+    {
+        PhysicsObject betoni = new PhysicsObject(50, 50);
+        betoni.Color = Color.Gray;
+        return betoni;
+    }
+
+    PhysicsObject LuoTiiliseina()
+    {
+        PhysicsObject tiili = new PhysicsObject(50, 50);
+        tiili.Color = Color.Brown;
+        return tiili;
+    }
+
+    PhysicsObject LuoPelaaja()
+    {
+        PhysicsObject pelaaja = new PhysicsObject(50, 50);
+        pelaaja.Image = pelaajaKuva;
+        return pelaaja;
+    }
+
+    void LuoKentta()
+    {
+        TileMap kentta = TileMap.FromFile("taso1.txt");
+        kentta['#'] = LuoBetoniseina;
+        kentta['='] = LuoTiiliseina;
+        kentta['p'] = LuoPelaaja;
+        kentta.Insert(50, 50);
+    }
+
+    public override void Begin()
+    {
+        LuoKentta();
+
+        // Kirjoita ohjelmakoodisi tähän
+
+        PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
+        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+
+    }
+}
